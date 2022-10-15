30fbbb7983b97374d7cf978e281ed2c59205b143
Blaine-McK <blainemckeever1993@gmail.com>
2022-10-15 18:19:20 +0100

    Added html

--- spend4cast/spend4castapp/urls.py
+++ spend4cast/spend4castapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    path('<int:user_id>/', views.detail, name='detail')
 ]
--- spend4cast/spend4castapp/views.py
+++ spend4cast/spend4castapp/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import User
 
 
 def index(request):
-    return HttpResponse("Hello, world! You're at the spend4castapp index.")
+    user_list = User.objects.values_list()
+    template = loader.get_template('spend4castapp/index.html')
+    context = {'user_list': user_list,}
+    return HttpResponse(template.render(context,request))    
+    #return HttpResponse("Hello, world! You're at the spend4castapp index.")
+
+def detail(request, user_id):
+    return(HttpResponse("You're lookig at user %s" % user_id))
